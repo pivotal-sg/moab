0fdb3a97304e38499620f85a08545366ec09e8eb
Jason Milkins + James Rivett-Carnac <pair+jm+jr@pivotal.io>
2016-04-19 10:07:09 +0800

    Generated maven project

---
+++ src/main/java/org/moab/MoabApplication.java
@@ -0,0 +1,12 @@
+package org.moab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoabApplication.class, args);
+	}
+}
