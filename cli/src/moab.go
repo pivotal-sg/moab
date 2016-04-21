141725ee689596aabafe547df7a5d2a5fd15ec6b
Jason Milkins + James Rivett-Carnac <pair+jm+jr@pivotal.io>
2016-04-21 11:10:48 +0800

    Handle error response in cli
    [#117881003]

--- cli/src/moab.go
+++ cli/src/moab.go
@@ -1,58 +1,62 @@
 package main
 
 import (
-  "fmt"
-  "net/http"
-  "io/ioutil"
-  "encoding/json"
-  "strings"
-  "flag"
-  "log"
-  "os"
+	"encoding/json"
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"strings"
 )
 
 type Create struct {
-  ClientName string `json:"clientName"`
-  ClientID string `json:"clientID"`
-  ClientDoB string `json:"clientDoB"`
+	ClientName string `json:"clientName"`
+	ClientID   string `json:"clientID"`
+	ClientDoB  string `json:"clientDoB"`
 }
 
 var createCommand Create
 
 func init() {
-  createCommand = Create{}
-  flag.StringVar(&createCommand.ClientName, "name", "", "Client Name")
-  flag.StringVar(&createCommand.ClientID, "id", "", "Client NRIC/FIN")
-  flag.StringVar(&createCommand.ClientDoB, "dob", "", "Client Date of Birth")
-  flag.Parse()
+	createCommand = Create{}
+	flag.StringVar(&createCommand.ClientName, "name", "", "Client Name")
+	flag.StringVar(&createCommand.ClientID, "id", "", "Client NRIC/FIN")
+	flag.StringVar(&createCommand.ClientDoB, "dob", "", "Client Date of Birth")
+	flag.Parse()
 }
 
 func main() {
-  var url string
+	var url string
 
-  url = os.Getenv("MOAB_URL")
+	url = os.Getenv("MOAB_URL")
 
-  if url == "" {
-    url = "http://localhost:8080/api/v1/account/"
-  }
-  payloadString, err := json.Marshal(createCommand)
-  if err != nil {
-    log.Fatalf("Failed to parse your magic command! %v\n", err.Error())
-  }
+	if url == "" {
+		url = "http://localhost:8080/api/v1/account/"
+	}
+	payloadString, err := json.Marshal(createCommand)
+	if err != nil {
+		log.Fatalf("Failed to parse your magic command! %v\n", err.Error())
+	}
 
-  payload := strings.NewReader(string(payloadString))
+	payload := strings.NewReader(string(payloadString))
 
-  req, _ := http.NewRequest("POST", url, payload)
+	req, _ := http.NewRequest("POST", url, payload)
 
-  req.Header.Add("content-type", "application/json")
-  req.Header.Add("cache-control", "no-cache")
+	req.Header.Add("content-type", "application/json")
+	req.Header.Add("cache-control", "no-cache")
 
-  res, _ := http.DefaultClient.Do(req)
+	res, err := http.DefaultClient.Do(req)
 
-  defer res.Body.Close()
-  body, _ := ioutil.ReadAll(res.Body)
+	if err != nil {
+		fmt.Println("No response")
+		os.Exit(1)
+	}
 
-  fmt.Println(res)
-  fmt.Println(string(body))
+	defer res.Body.Close()
+	body, _ := ioutil.ReadAll(res.Body)
 
+	fmt.Println(res)
+	fmt.Println(string(body))
 }
